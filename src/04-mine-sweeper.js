8817feb4e75933d288af08a5266ea00b7f4e9c28
ogobelbel <74075526+ogobelbel@users.noreply.github.com>
2021-04-09 12:26:50 +0300

    Update 04-mine-sweeper.js

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,62 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  let count = 0;
+  const arr = matrix.slice();
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i][j] === true) {
+        arr[i][j] = 33;
+      }
+    }
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix.length; j++) {
+      if ((arr[i][j] !== 33) && ((i !== 0 && arr[i - 1][j] === 33)
+      || (i !== matrix.length - 1 && arr[i + 1][j] === 33)
+      || (j !== 0 && arr[i][j - 1] === 33) || (j !== matrix.length - 1 && arr[i][j + 1] === 33)
+      || (i !== 0 && j !== 0 && arr[i - 1][j - 1] === 33)
+      || (i !== matrix.length - 1 && j !== matrix.length - 1 && arr[i + 1][j + 1] === 33)
+      || (i !== 0 && j !== matrix.length - 1 && arr[i - 1][j + 1] === 33)
+      || (j !== 0 && i !== matrix.length - 1 && arr[i + 1][j - 1] === 33))) {
+        if (i !== 0 && arr[i - 1][j] === 33) {
+          count++;
+        }
+        if (i !== matrix.length - 1 && arr[i + 1][j] === 33) {
+          count++;
+        }
+        if (j !== 0 && arr[i][j - 1] === 33) {
+          count++;
+        }
+        if (j !== matrix.length - 1 && arr[i][j + 1] === 33) {
+          count++;
+        }
+        if (i !== 0 && j !== 0 && arr[i - 1][j - 1] === 33) {
+          count++;
+        }
+        if (i !== matrix.length - 1 && j !== matrix.length - 1 && arr[i + 1][j + 1] === 33) {
+          count++;
+        }
+        if (i !== 0 && j !== matrix.length - 1 && arr[i - 1][j + 1] === 33) {
+          count++;
+        }
+        if (j !== 0 && i !== matrix.length - 1 && arr[i + 1][j - 1] === 33) {
+          count++;
+        }
+        arr[i][j] = count;
+        count = 0;
+      }
+    }
+  }
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i][j] === 33) {
+        arr[i][j] = 1;
+      }
+    }
+  }
+  return arr;
 }
 
 module.exports = minesweeper;
