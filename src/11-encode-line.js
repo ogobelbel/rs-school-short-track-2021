29704125c378cdb652d000c6a6af20720fdbdb76
ogobelbel <74075526+ogobelbel@users.noreply.github.com>
2021-04-11 21:21:28 +0300

    Update 11-encode-line.js

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,23 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const res = [];
+  let count = 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      count++;
+    }
+    if (str[i] !== str[i + 1] && count === 1) {
+      res.push(str[i]);
+      count = 1;
+    }
+    if (str[i] !== str[i + 1] && count > 1) {
+      res.push(`${count}${str[i]}`);
+      count = 1;
+    }
+  }
+  return res.join('');
 }
 
 module.exports = encodeLine;
