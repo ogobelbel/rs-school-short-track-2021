71c25c45976db4ac2e751c30b90c2324aa2d42ae
ogobelbel <74075526+ogobelbel@users.noreply.github.com>
2021-04-12 17:07:23 +0300

    Update 12-dns-stats.js

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,37 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  const data = {};
+
+  for (let i = 0; i < domains.length; i++) {
+    const elem = domains[i];
+    let strPrev = '';
+    let arr = [];
+    let arr2 = [];
+    let str = '';
+    for (let j = elem.length; j >= 0; j--) {
+      if (elem[j] !== '.') {
+        arr.push(elem[j]);
+      }
+      if (elem[j] === '.' || j === 0) {
+        arr.push('.');
+        arr2 = arr.slice();
+        arr2.reverse();
+        str = arr2.join('');
+        strPrev += str;
+        str = '';
+        arr2 = [];
+        arr = [];
+        if (typeof data[strPrev] !== 'undefined') {
+          data[strPrev]++;
+        } else {
+          data[strPrev] = 1;
+        }
+      }
+    }
+  }
+  return data;
 }
 
 module.exports = getDNSStats;
